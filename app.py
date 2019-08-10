dce7efd34f18b6414d5382148ce4905586f34b1c
mikesdev1 <mikes.dev1@gmail.com>
2019-08-10 08:46:15 -0400

    added flow for signup

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, json
+from flask.ext.mysql import MySQL
+from wekzeug import generate_password_hash, check_password_hash
+
+
+
+mysql = MySQL()
+ 
+
+
 app = Flask(__name__)
+
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'jay'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
 
 @app.route("/")
 def main():
@@ -8,6 +27,31 @@ def main():
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
+@app.route('/signUp',methods=['POST'])
+def signUp():
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+
+    _hashed_password = generate_password_hash(_password)
+    cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+
+    data = cursor.fetchall()
+ 
+    if len(data) is 0:
+        conn.commit()
+        return json.dumps({'message':'User created successfully !'})
+    else:
+        return json.dumps({'error':str(data[0])})
+
+
 
 if __name__ == "__main__":
     app.run()
